cc0a6bb86f9c0b35911800e18c14999d66cb35fe
arun <arunsabat.7@gmail.com>
2020-06-21 10:13:40 +0530

    Updates

---
+++ precache-manifest.7bc755bbc1b7bf0fe4b8519ffdc0a9a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d195d09636b52f2d8777b40b5c0dbef6",
+    "url": "/React-Food-Recipe/index.html"
+  },
+  {
+    "revision": "9a8f06c1dfa1ae3bccb9",
+    "url": "/React-Food-Recipe/static/css/main.274f0c1f.chunk.css"
+  },
+  {
+    "revision": "d2d211fc95951708fa49",
+    "url": "/React-Food-Recipe/static/js/2.0ef46cd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Food-Recipe/static/js/2.0ef46cd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a8f06c1dfa1ae3bccb9",
+    "url": "/React-Food-Recipe/static/js/main.1b02cced.chunk.js"
+  },
+  {
+    "revision": "5f2da5eac95468a7fe69",
+    "url": "/React-Food-Recipe/static/js/runtime-main.c8518eb0.js"
+  }
+]);
